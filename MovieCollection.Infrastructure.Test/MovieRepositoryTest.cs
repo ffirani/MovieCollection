f3fd131220a674c64985db280ecbc58e277849eb
Farshid <f.f.irani@live.com>
2024-06-24 07:21:34 +0330

    Add MovieRepositoryTest

---
+++ MovieCollection.Infrastructure.Test/MovieRepositoryTest.cs
@@ -0,0 +1,11 @@
+namespace MovieCollection.Infrastructure.Test
+{
+    public class MovieRepositoryTest
+    {
+        [Fact]
+        public void CreateAsync_valid_movie_entity_return_created_entity_id()
+        {
+
+        }
+    }
+}
